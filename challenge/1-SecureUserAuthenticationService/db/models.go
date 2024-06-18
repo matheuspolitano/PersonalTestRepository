c429d97e552385624657e2a3d4e44323f5162017
Matheus Politano <matheuspolitano1@gmail.com>
2024-06-18 10:08:16 -0300

    add create user and get user id

--- challenge/1-SecureUserAuthenticationService/db/models.go
+++ challenge/1-SecureUserAuthenticationService/db/models.go
@@ -5,11 +5,11 @@ import (
 )
 
 type User struct {
-	UserID              int       `gorm:"primaryKey;column:user_id"`
-	FirstName           string    `gorm:"first_name"`
-	LastName            string    `gorm:"last_name"`
-	Email               string    `gorm:"email"`
-	HashPassword        string    `gorm:"hash_password"`
-	CreatedAt           time.Time `gorm:"created_at"`
-	PasswordLastChanged time.Time `gorm:"password_last_changed"`
+	UserID              int       `gorm:"primaryKey;autoIncrement;column:user_id"`
+	FirstName           string    `gorm:"not null;column:first_name"`
+	LastName            string    `gorm:"not null;column:last_name"`
+	Email               string    `gorm:"not null;column:email;unique"`
+	HashPassword        string    `gorm:"not null;column:hash_password"`
+	CreatedAt           time.Time `gorm:"not null;column:created_at"`
+	PasswordLastChanged time.Time `gorm:"not null;column:password_last_changed"`
 }
--- challenge/1-SecureUserAuthenticationService/db/queries.go
+++ challenge/1-SecureUserAuthenticationService/db/queries.go
@@ -1,6 +1,11 @@
 package db
 
-import "errors"
+import (
+	"errors"
+	"time"
+
+	"github.com/matheuspolitano/PersonalTestRepository/challenge/1-SecureUserAuthenticationService/utils"
+)
 
 var NotFound = errors.New("now was found the obejct")
 
@@ -11,4 +16,32 @@ func (store *Store) GetUserId(id int) (*User, error) {
 		return nil, NotFound
 	}
 	return user, nil
+}
+
+func (store *Store) CreateUser(firstName string, lastName string, email string, password string) (*User, error) {
+	hashedPassword, err := utils.HashPassword(password)
+	if err != nil {
+		return nil, err
+	}
+	var user = &User{
+		FirstName:           firstName,
+		LastName:            lastName,
+		Email:               email,
+		HashPassword:        hashedPassword,
+		CreatedAt:           time.Now(),
+		PasswordLastChanged: time.Now(),
+	}
+
+	result := store.db.Create(&user)
+
+	if err = result.Error; err != nil {
+		return nil, err
+	}
+
+	if result.RowsAffected == 0 {
+		return nil, errors.New("row does not insert")
+	}
+
+	return user, nil
+
 }
