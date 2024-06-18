182eec328e61dd98edb47f4ba58c01cae43879c6
Matheus Politano <matheuspolitano1@gmail.com>
2024-06-18 10:49:55 -0300

    add test to change password

--- challenge/1-SecureUserAuthenticationService/db/user_test.go
+++ challenge/1-SecureUserAuthenticationService/db/user_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestCreateUse(t *testing.T) {
+func TestCreateUser(t *testing.T) {
 	firstName := utils.RandString(10)
 	lastName := utils.RandString(10)
 	email := utils.RandString(60)
@@ -22,6 +22,40 @@ func TestCreateUse(t *testing.T) {
 	require.Equal(t, user.Email, email)
 
 	isValid := utils.CheckPassword(password, user.HashPassword)
+	require.True(t, isValid)
+
+	err = testStore.DeleteById(user.UserID)
+	require.NoError(t, err)
+}
+
+func TestChangePassword(t *testing.T) {
+	firstName := utils.RandString(10)
+	lastName := utils.RandString(10)
+	email := utils.RandString(60)
+	password := utils.RandString(10)
+	newPassword := utils.RandString(10)
+
+	user, err := testStore.CreateUser(firstName, lastName, email, password)
+	require.NoError(t, err)
+	require.NotEmpty(t, user)
+
+	require.Equal(t, user.FirstName, firstName)
+	require.Equal(t, user.LastName, lastName)
+	require.Equal(t, user.Email, email)
+
+	isValid := utils.CheckPassword(password, user.HashPassword)
+	require.True(t, isValid)
+
+	err = testStore.ChangePassword(user.UserID, newPassword)
+	require.NoError(t, err)
+
+	userUpdated, err := testStore.GetUserId(user.UserID)
+	require.NoError(t, err)
+
+	isValid = utils.CheckPassword(password, userUpdated.HashPassword)
+	require.False(t, isValid)
+
+	isValid = utils.CheckPassword(newPassword, userUpdated.HashPassword)
 	require.True(t, isValid)
 
 	err = testStore.DeleteById(user.UserID)
